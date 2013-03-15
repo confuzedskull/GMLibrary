64f6c88980e9bc2dca7508258d3e4b3e2a6aa26a
James Nakano <jfunkymonkey007@gmail.com>
2013-03-14 19:53:34 -0700

    added get methods and javadoc to class Action

--- src/GameModel/Actions/Action.java
+++ src/GameModel/Actions/Action.java
@@ -2,7 +2,7 @@
 package GameModel.Actions;
 
 /**
- * Action
+ * Every action has a cost, difficulty level, and requires people to perform the action
  * @author James
  */
 public class Action {
@@ -18,9 +18,31 @@ public class Action {
     public Action()
     {
         cost=0;
-        requiredPeople=0;
+        requiredPeople=1;
         difficulty=0;
     }
+    /**
+     * Gets the cost of the action.
+     * @return the cost of the action
+     */
+    public int getCost() {
+        return cost;
+    }
+    /**
+     * Gets the required number of people
+     * @return the number of required people
+     */
+    public int getRequiredPeople() {
+        return requiredPeople;
+    }
+    /**
+     * Gets the level of difficulty
+     * @return an integer representing the level of difficulty
+     */
+    public int getDifficulty() {
+        return difficulty;
+    }
+    
    
     
 }
