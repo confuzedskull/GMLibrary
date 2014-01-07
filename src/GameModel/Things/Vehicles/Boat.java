aec661b9150637964bb9eb6c5f83bf103f6cf16d
James <James@James-Vaio>
2014-01-07 11:52:50 -0500

    Added Boat class and added refuel method to Vehicle

---
+++ src/GameModel/Things/Vehicles/Boat.java
@@ -0,0 +1,12 @@
+
+package GameModel.Things.Vehicles;
+
+/**
+ * A boat is a vehicle that can travel on water.
+ * It can transport small or large things, depending on the size.
+ * @author James
+ */
+public class Boat extends Vehicle
+{
+    
+}
